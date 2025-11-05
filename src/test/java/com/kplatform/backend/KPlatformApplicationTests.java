88c2aead9d574f676aab30f587be96e8fb40f855
k <dlrlwjd1313@naver.com>
2025-11-05 16:22:16 +0900

    첫 커밋 2025 11 05 이기정 사이드 벡엔드 시작

---
+++ src/test/java/com/kplatform/backend/KPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kplatform.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
